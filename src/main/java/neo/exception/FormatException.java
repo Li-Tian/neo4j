9e1f4406b5fa105aa986c267869f1158d60d5e83
Jin Qiao <jinqiao@neo.org>
2019-03-01 18:26:44 +0800

    Adding FormatException contructor

--- src/main/java/neo/exception/FormatException.java
+++ src/main/java/neo/exception/FormatException.java
@@ -1,4 +1,17 @@
 package neo.exception;
 
+import neo.log.tr.TR;
+
 public class FormatException extends RuntimeException {
+    public FormatException(String info) {
+        super(info);
+        TR.enter();
+        TR.exit();
+    }
+
+    public FormatException() {
+        super();
+        TR.enter();
+        TR.exit();
+    }
 }
