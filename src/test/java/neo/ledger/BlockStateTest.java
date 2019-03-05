9d9b004f83dd3f72bf235f7d21dec3cbde1807a2
luchuan <luchuan@neo.org>
2019-03-05 09:52:15 +0800

    1. fix BlockStateTest unit test error

--- src/test/java/neo/ledger/BlockStateTest.java
+++ src/test/java/neo/ledger/BlockStateTest.java
@@ -144,27 +144,23 @@ public class BlockStateTest {
 
     @Test
     public void toJson() {
-        TR.fixMe("这个测试没有通过！");
-//        BlockState blockState = new BlockState();
-//        blockState.systemFeeAmount = 100;
-//        blockState.trimmedBlock = new TrimmedBlock();
-//        blockState.trimmedBlock.consensusData = new Ulong(10);
-//        blockState.trimmedBlock.version = new Uint(1);
-//        blockState.trimmedBlock.prevHash = UInt256.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01");
-//        blockState.trimmedBlock.merkleRoot = UInt256.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff02");
-//        blockState.trimmedBlock.timestamp = new Uint(1568489959);
-//        blockState.trimmedBlock.index = new Uint(10);
-//        blockState.trimmedBlock.nextConsensus = UInt160.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff01");
-//        blockState.trimmedBlock.witness = new Witness();
-//        blockState.trimmedBlock.witness.verificationScript = new byte[]{0x01, 0x02};
-//        blockState.trimmedBlock.witness.invocationScript = new byte[]{0x3, 0x04};
-//        blockState.trimmedBlock.hashes = new UInt256[]{new MinerTransaction().hash()};
-//
-//        JsonObject jsonObject = blockState.toJson();
-//        Assert.assertEquals("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff01", jsonObject.get("script_hash").getAsString());
-//        Assert.assertEquals("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff01", jsonObject.get("script_hash").getAsString());
-//
-//        Assert.assertEquals(100, jsonObject.get("sysfee_amount").getAsLong());
-//        Assert.assertEquals("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01", jsonObject.getAsJsonObject("trimmed").get("previousblockhash").getAsString());
+        BlockState blockState = new BlockState();
+        blockState.systemFeeAmount = 100;
+        blockState.trimmedBlock = new TrimmedBlock();
+        blockState.trimmedBlock.consensusData = new Ulong(10);
+        blockState.trimmedBlock.version = new Uint(1);
+        blockState.trimmedBlock.prevHash = UInt256.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01");
+        blockState.trimmedBlock.merkleRoot = UInt256.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff02");
+        blockState.trimmedBlock.timestamp = new Uint(1568489959);
+        blockState.trimmedBlock.index = new Uint(10);
+        blockState.trimmedBlock.nextConsensus = UInt160.parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff01");
+        blockState.trimmedBlock.witness = new Witness();
+        blockState.trimmedBlock.witness.verificationScript = new byte[]{0x01, 0x02};
+        blockState.trimmedBlock.witness.invocationScript = new byte[]{0x3, 0x04};
+        blockState.trimmedBlock.hashes = new UInt256[]{new MinerTransaction().hash()};
+
+        JsonObject jsonObject = blockState.toJson();
+        Assert.assertEquals(100, jsonObject.get("sysfee_amount").getAsLong());
+        Assert.assertEquals("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01", jsonObject.getAsJsonObject("trimmed").get("previousblockhash").getAsString());
     }
 }
