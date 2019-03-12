92e460648ee236b24576db72eee626a91c70e840
Jin Qiao <jinqiao@neo.org>
2019-03-12 17:15:01 +0800

    Add SmartContract Contract

--- src/main/java/neo/smartcontract/Contract.java
+++ src/main/java/neo/smartcontract/Contract.java
@@ -1,6 +1,9 @@
 package neo.smartcontract;
 
+import java.math.BigInteger;
+import java.util.Arrays;
 
+import neo.UInt160;
 import neo.log.tr.TR;
 import neo.vm.OpCode;
 import neo.vm.ScriptBuilder;
@@ -10,6 +13,64 @@ import neo.cryptography.ecc.ECPoint;
  * 合约类，提供了合约的构造方法，以及创建多方签名和单签合约的方法
  */
 public class Contract {
+    public byte[] script;
+    public ContractParameterType[] parameterList;
+    private String _address;
+
+    public String address() {
+        TR.enter();
+        if (_address == null) {
+            _address = scriptHash().toAddress();
+        }
+        return TR.exit(_address);
+    }
+
+    private UInt160 _scriptHash;
+
+    public UInt160 scriptHash() {
+        if (_scriptHash == null) {
+            _scriptHash = UInt160.parseToScriptHash(script);
+        }
+        return _scriptHash;
+    }
+
+    public Contract(ContractParameterType[] parameterList, byte[] redeemScript) {
+        this.script = redeemScript;
+        this.parameterList = parameterList;
+    }
+
+    public static Contract create(ContractParameterType[] parameterList, byte[] redeemScript) {
+        TR.enter();
+        return TR.exit(new Contract(parameterList, redeemScript));
+    }
+
+    public static Contract createMultiSigContract(int m, ECPoint[] publicKeys) {
+        TR.enter();
+        ContractParameterType[] output = new ContractParameterType[m];
+        Arrays.fill(output, ContractParameterType.Signature);
+        return TR.exit(new Contract(output, createMultiSigRedeemScript(m, publicKeys)));
+    }
+
+    public static byte[] createMultiSigRedeemScript(int m, ECPoint[] publicKeys) {
+        TR.enter();
+        if (!(1 <= m && m <= publicKeys.length && publicKeys.length <= 1024)) {
+            TR.exit();
+            throw new IllegalArgumentException();
+        }
+        ScriptBuilder sb = new ScriptBuilder();
+        sb.emitPush(BigInteger.valueOf(m));
+        for (ECPoint publicKey : Arrays.stream(publicKeys).sorted(ECPoint::compareTo).toArray(ECPoint[]::new)) {
+            sb.emitPush(publicKey.getEncoded(true));
+        }
+        sb.emitPush(BigInteger.valueOf(publicKeys.length));
+        sb.emit(OpCode.CHECKMULTISIG);
+        return TR.exit(sb.toArray());
+    }
+
+    public static Contract CreateSignatureContract(ECPoint publicKey) {
+        TR.enter();
+        return TR.exit(new Contract(new ContractParameterType[]{ContractParameterType.Signature}, createSignatureRedeemScript(publicKey)));
+    }
 
     /**
      * 获取公钥对应的脚本字节码
@@ -24,5 +85,4 @@ public class Contract {
         sb.emit(OpCode.CHECKSIG);
         return TR.exit(sb.toArray());
     }
-
 }
