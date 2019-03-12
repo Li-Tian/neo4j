e2689398bee4c1b80ae4c0352b6ab4ee3f6b2136
Jin Qiao <jinqiao@neo.org>
2019-03-12 10:15:58 +0800

    Fix SCrypt

--- src/main/java/neo/cryptography/SCrypt.java
+++ src/main/java/neo/cryptography/SCrypt.java
@@ -3,7 +3,7 @@ package neo.cryptography;
 import neo.log.tr.TR;
 
 public class SCrypt {
-    public static byte[] DeriveKey(byte[] password, byte[] salt, int N, int r, int p, int derivedKeyLength) {
+    public static byte[] deriveKey(byte[] password, byte[] salt, int N, int r, int p, int derivedKeyLength) {
         TR.enter();
         return TR.exit(org.bouncycastle.crypto.generators.SCrypt.generate(password, salt, N, r, p, derivedKeyLength));
     }
