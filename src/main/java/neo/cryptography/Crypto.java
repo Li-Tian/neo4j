451d931940dca05df74efbacd155f231b5970779
luchuan <luchuan@neo.org>
2019-03-19 11:29:29 +0800

    1. complete crypto add add unit test

--- src/main/java/neo/cryptography/Crypto.java
+++ src/main/java/neo/cryptography/Crypto.java
@@ -1,112 +1,109 @@
 package neo.cryptography;
 
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.crypto.signers.ECDSASigner;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.BigIntegers;
 
-import sun.security.ec.ECPrivateKeyImpl;
-import sun.security.ec.ECPublicKeyImpl;
-
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.math.BigInteger;
 import java.security.Security;
-import java.security.Signature;
-import java.security.SignatureException;
 
 import neo.cryptography.ecc.ECC;
-import neo.log.tr.TR;
+import neo.log.notr.TR;
 import neo.vm.ICrypto;
 
 import static neo.cryptography.Helper.ripeMD160;
 import static neo.cryptography.Helper.sha256;
 
+/**
+ * Crypto implement the ICrypto interface in NVM, provides hash160, hash256, sign and verifySign
+ * methods.
+ */
 public class Crypto implements ICrypto {
 
+    /**
+     * default instance of Crypto
+     */
     public static final Crypto Default = new Crypto();
 
-    private static final String SIGNATURE_ALGORITHM = "SHA256withECDSA";
-
     static {
-        //加入BouncyCastleProvider的支持
+        //add BouncyCastleProvider support
         Security.addProvider(new BouncyCastleProvider());
     }
 
+    /**
+     * hash160 method
+     */
     @Override
     public byte[] hash160(byte[] message) {
+        TR.enter();
         try {
             byte[] hash256 = sha256(message);
             byte[] ripeMD160 = ripeMD160(hash256);
-            return ripeMD160;
+            return TR.exit(ripeMD160);
         } catch (Exception e) {
-            TR.error(e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public byte[] hash256(byte[] message) {
-        try {
-            return sha256(sha256(message));
-        } catch (Exception e) {
-            TR.error(e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public boolean verifySignature(byte[] message, byte[] signature, byte[] pubkey) {
-        // TODO 尚未验证
-        TR.enter();
-        if (pubkey.length == 33 && (pubkey[0] == 0x02 || pubkey[0] == 0x03)) {
-            try {
-                pubkey = ECC.Secp256r1.getCurve().decodePoint(pubkey).getEncoded(false);
-                byte[] tmp = new byte[pubkey.length - 1];
-                System.arraycopy(pubkey, 1, tmp, 0, pubkey.length - 1);
-                pubkey = tmp;
-                // C# code
-                //  pubkey = Cryptography.ECC.ECPoint.DecodePoint(pubkey, Cryptography.ECC.ECCurve.Secp256r1).EncodePoint(false).Skip(1).ToArray();
-            } catch (Exception e) {
-                return false;
-            }
-        } else if (pubkey.length == 65 && pubkey[0] == 0x04) {
-            byte[] tmp = new byte[64];
-            System.arraycopy(pubkey, 1, tmp, 0, 64);
-            pubkey = tmp;
-        } else if (pubkey.length != 64) {
-            throw new IllegalArgumentException();
-        }
-
-        try {
-            Signature sign = Signature.getInstance(SIGNATURE_ALGORITHM);
-            PublicKey key = new ECPublicKeyImpl(pubkey);
-            sign.initVerify(key);
-            sign.update(message);
-            return TR.exit(sign.verify(signature));
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
             TR.error(e);
             throw new RuntimeException(e);
         }
     }
 
     /**
-     * 数据签名 (ECDSA 方法进行签名)
-     *
-     * @param message    待签名数据
-     * @param privateKey 私钥
-     * @return 签名数据
+     * hash256 method
      */
-    public byte[] sign(byte[] message, byte[] privateKey) {
-        // TODO 尚未验证
+    @Override
+    public byte[] hash256(byte[] message) {
+        TR.enter();
         try {
-            TR.enter();
-            Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
-            PrivateKey key = new ECPrivateKeyImpl(privateKey);
-            signature.initSign(key);
-            signature.update(message);
-            return TR.exit(signature.sign());
-        } catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
+            return TR.exit(sha256(sha256(message)));
+        } catch (Exception e) {
             TR.error(e);
             throw new RuntimeException(e);
         }
     }
+
+
+    /**
+     * sign the message with ECC ecdsa, using Secp256r1 curve
+     *
+     * @param message    message to be signed
+     * @param privateKey private key
+     * @return signature data
+     */
+    public byte[] sign(byte[] message, byte[] privateKey) {
+        TR.enter();
+        ECDSASigner signer = new ECDSASigner();
+        BigInteger d = new BigInteger(1, privateKey);
+        ECPrivateKeyParameters privateKeyParameters = new ECPrivateKeyParameters(d, ECC.Secp256r1);
+        signer.init(true, privateKeyParameters);
+
+        BigInteger[] bi = signer.generateSignature(sha256(message));
+        byte[] signature = new byte[64];
+        System.arraycopy(BigIntegers.asUnsignedByteArray(32, bi[0]), 0, signature, 0, 32);
+        System.arraycopy(BigIntegers.asUnsignedByteArray(32, bi[1]), 0, signature, 32, 32);
+        return TR.exit(signature);
+    }
+
+    /**
+     * verify the signature
+     *
+     * @param message   the signed message
+     * @param signature signature of the message
+     * @param publicKey public key
+     * @return true - verify success, otherwise false
+     */
+    @Override
+    public boolean verifySignature(byte[] message, byte[] signature, byte[] publicKey) {
+        TR.enter();
+        ECDSASigner signer = new ECDSASigner();
+        org.bouncycastle.math.ec.ECPoint publicPoint = ECC.Secp256r1.getCurve().decodePoint(publicKey);
+        ECPublicKeyParameters publicKeyParameters = new ECPublicKeyParameters(publicPoint, ECC.Secp256r1);
+        signer.init(false, publicKeyParameters);
+
+        byte[] hash = sha256(message);
+        BigInteger r = BigIntegers.fromUnsignedByteArray(signature, 0, 32);
+        BigInteger s = BigIntegers.fromUnsignedByteArray(signature, 32, 32);
+        return TR.exit(signer.verifySignature(hash, r, s));
+    }
+
 }
