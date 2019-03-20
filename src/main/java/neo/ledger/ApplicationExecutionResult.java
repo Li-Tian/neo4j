92cca076b07b1b4527b7b1f878fdad2d3263b0f5
Jin Qiao <jinqiao@neo.org>
2019-03-20 12:04:16 +0800

    Code renew

--- src/main/java/neo/ledger/ApplicationExecutionResult.java
+++ src/main/java/neo/ledger/ApplicationExecutionResult.java
@@ -1,5 +1,17 @@
 package neo.ledger;
 
-public class ApplicationExecutionResult {
+import neo.Fixed8;
+import neo.smartcontract.NotifyEventArgs;
+import neo.smartcontract.TriggerType;
+import neo.UInt160;
+import neo.vm.StackItem;
+import neo.vm.VMState;
 
+public class ApplicationExecutionResult {
+    public TriggerType trigger;
+    public UInt160 scriptHash;
+    public VMState vmState;
+    public Fixed8 gasConsumed;
+    public StackItem[] stack;
+    public NotifyEventArgs[] notifications;
 }
