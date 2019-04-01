1bb4a3664f2bb1cf2e8b21669d0cc5ca1f9826f5
Jin Qiao <jinqiao@neo.org>
2019-04-01 10:42:57 +0800

    Add HttpServletResponse to IRpcPlugin

--- src/main/java/neo/plugins/IRpcPlugin.java
+++ src/main/java/neo/plugins/IRpcPlugin.java
@@ -4,7 +4,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface IRpcPlugin {
-    JsonObject onProcess(HttpServletRequest req, String method, JsonArray _params);
+    JsonObject onProcess(HttpServletRequest req, HttpServletResponse res, String method, JsonArray _params);
 }
