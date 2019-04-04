ae34944e4741f7af3b7d8d88cda430cc2a9f7a45
luchuan <luchuan@neo.org>
2019-04-04 10:28:34 +0800

    1. extend DBHelper.find method

--- src/main/java/neo/persistence/leveldb/DBHelper.java
+++ src/main/java/neo/persistence/leveldb/DBHelper.java
@@ -44,4 +44,19 @@ public class DBHelper {
         return TR.exit(list);
     }
 
+
+    /**
+     * query by key prefix
+     *
+     * @param db        leveldb
+     * @param prefix    prefix
+     * @param key       key
+     * @param generator value generator eg: (keyBytes, valueBytes) -> item
+     * @param <R>       value type
+     * @return Collection<R>, empty set will be return if not found
+     */
+    public static <R> Collection<R> find(DB db, byte prefix, byte[] key, BiFunction<byte[], byte[], R> generator) {
+        byte[] keyBytes = BitConverter.merge(prefix, key);
+        return find(db, keyBytes, generator);
+    }
 }
